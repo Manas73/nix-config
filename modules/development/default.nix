049e40a90d008be0043a5d467ceb95f95896efc8
Manas Sambare <manas.sambare@gmail.com>
2024-09-29 15:01:32 +0530

    [Development] Rename option sublime4 to sublime

--- modules/development/default.nix
+++ modules/development/default.nix
@@ -1,7 +1,7 @@
 { pkgs, lib, development_apps, ... }:
 let
   functions = import ../functions.nix { inherit pkgs lib; };
-  development_options = [ "gitkraken" "pycharm-professional" "datagrip" "postman" "sublime4" ];
+  development_options = [ "gitkraken" "pycharm-professional" "datagrip" "postman" "sublime" ];
 in
 functions.makeModuleConfig {
   options = development_options;
