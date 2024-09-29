80ce677e2631ed44b660eb3352457767ce1b4077
Manas Sambare <manas.sambare@gmail.com>
2024-09-29 14:34:13 +0530

    [Home] Dynamically add Home Modules

--- users/ms-nixos/home.nix
+++ users/ms-nixos/home.nix
@@ -1,14 +1,23 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
 
+let
+  # Get all entries in the current directory
+  entries = builtins.readDir ./.;
+
+  # Filter out non-directories and create import paths
+  imports = builtins.map
+    (name: "./${name}")
+    (builtins.filter
+      (name: entries.${name} == "directory")
+      (builtins.attrNames entries));
+in
+{
   imports = [
-
     ../base_user.nix
-
-  ];
+  ] ++ imports;
 
   programs.git = {
     userName = "Manas73";
     userEmail = "manas.sambare@gmail.com";
   };
-
 }
