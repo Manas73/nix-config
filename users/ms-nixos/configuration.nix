5a0a9e411322e0236f23ffffc1142658c81912bf
Manas Sambare <manas.sambare@gmail.com>
2024-10-06 01:57:12 +0530

    [SOPS] Remove Password from user config

--- users/ms-nixos/configuration.nix
+++ users/ms-nixos/configuration.nix
@@ -3,12 +3,8 @@
     ../../system_modules
     ];
 
-    sops.secrets.ms-nixos-password.neededForUsers = true;
-    users.mutableUsers = false;   # Requried for password to be set via sops during system activation!
-
     users.users.ms-nixos = {
       isNormalUser = true;
-      hashedPasswordFile = config.sops.secrets.ms-nixos-password.path;
       shell = pkgs.${user_settings.default_shell};
       description = user_settings.full_name;
       extraGroups = [ "networkmanager" "wheel" ];
