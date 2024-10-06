b79a4e5705be0a3ab77f7af39df8c8c3340e1c9a
Manas Sambare <manas.sambare@gmail.com>
2024-10-06 17:57:40 +0530

    [Font] Add Roboto

--- system_modules/font/default.nix
+++ system_modules/font/default.nix
@@ -4,6 +4,7 @@
   fonts.packages = with pkgs; [
     (nerdfonts.override { fonts = [ "SourceCodePro" "JetBrainsMono" ]; })
     powerline
+    roboto
     material-icons
     material-design-icons
   ];
