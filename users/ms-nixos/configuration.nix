47e64bd7ed26220e28176dbf5c34e52ad24a918a
Manas Sambare <manas.sambare@gmail.com>
2024-10-13 18:30:07 +0530

    [ms-nixos] Enable gnome-polkit service

--- users/ms-nixos/configuration.nix
+++ users/ms-nixos/configuration.nix
@@ -62,4 +62,20 @@
       openssh.authorizedKeys.keys = config.users.users.${user_settings.username}.openssh.authorizedKeys.keys;
     };
 
+    systemd = {
+      user.services.polkit-gnome-authentication-agent-1 = {
+        description = "polkit-gnome-authentication-agent-1";
+        wantedBy = [ "graphical-session.target" ];
+        wants = [ "graphical-session.target" ];
+        after = [ "graphical-session.target" ];
+        serviceConfig = {
+            Type = "simple";
+            ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+            Restart = "on-failure";
+            RestartSec = 1;
+            TimeoutStopSec = 10;
+          };
+      };
+    };
+
 }
