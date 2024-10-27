3468964bff90ed3058913c3590ca09d04915b976
Manas Sambare <manas.sambare@gmail.com>
2024-10-27 23:04:33 +0530

    [Script] Update Sync Darwin Work

--- sync-darwin-work.sh
+++ sync-darwin-work.sh
@@ -2,3 +2,5 @@
 
 # Sync Darwin Configuration
 darwin-rebuild switch --flake .#manass-KY4RYYHFF6 --show-trace 2>&1 | tee home-rebuild.log;
+
+home-manager switch --flake .#manas.s --show-trace 2>&1 | tee home-rebuild.log
