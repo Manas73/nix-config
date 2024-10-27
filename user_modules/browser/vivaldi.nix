94a64057b69348a7f739f9087ccd5a759c7df810
Manas Sambare <manas.sambare@gmail.com>
2024-10-27 21:33:42 +0530

    [Browser] Remove Incorrect Cask Implementation

--- user_modules/browser/vivaldi.nix
+++ user_modules/browser/vivaldi.nix
@@ -5,13 +5,11 @@
     };
 
     config = lib.mkIf config.vivaldi.enable {
-        home.packages = lib.mkIf (pkgs.stdenv.isLinux) (with pkgs; [
-            (vivaldi.override {
-                proprietaryCodecs = true;
-                enableWidevine = true;
-            })
-        ]);
-
-        homebrew.casks = lib.mkIf pkgs.stdenv.isDarwin [ "vivaldi" ];
+        home.packages = with pkgs; [
+           (vivaldi.override {
+             proprietaryCodecs = true;
+             enableWidevine = true;
+           })
+       ];
     };
 }
